90dfaa62a406e96bca804c5781888836b50a25c8
g0atje <willemsencheyenne@gmail.com>
2026-09-18 23:31:02 +0200

    Update Mastermind.java
    placed the variabels and user input-output in it

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,10 +1,42 @@
 package mastermind;
 
+import java.util.Scanner;
+
 public class Mastermind {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
+	   String  codemaker; 
+       String  codekraker; 
+       String  code;
+       String  kleur; 
+       
+       int     positie;
+       int     vak;
+       int     poging;
+       int     zwartePinnen;
+       int     wittePinnen;
+       
+       boolean geraakt;
+       boolean gekraakt; 
+       
+       
+	   Scanner scanner = new Scanner(System.in);
+	   System.out.println("Kies een codemaker:");
+	   codemaker = scanner.nextLine();
+	   System.out.println("Kies een codekraker:");
+	   codekraker = scanner.nextLine();
+	   System.out.println("Kies een code:");
+	   code = scanner.nextLine();
+	   System.out.println("Kies een kleur:");
+	   kleur = scanner.nextLine();
+	   System.out.println("kies een positie 1 t/m 4:");
+	   positie = scanner.nextInt();
+	   
+		
+		
 	}
 
 }
